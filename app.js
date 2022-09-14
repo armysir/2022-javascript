4114b307c875e173cace612dead77e7778381496
armysir <102854930+armysir@users.noreply.github.com>
2022-09-14 16:53:22 +0900

    9-14
    로그인 화면 만들다 맘

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(12414)
+const loginForm =document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector(".greeting");
+
+
+
+function loginSubmit(event){
+   event.preventDefault();
+   const userName=loginInput.value;
+   loginForm.classList.add("hidden");
+   localStorage.setItem("username",userName);
+   greeting.innerText="안녕하세요, "+userName+"님";
+   greeting.classList.remove("hidden");
+
+}
+
+loginForm.addEventListener("submit",loginSubmit)
